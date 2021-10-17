21b7bd02af25c134ad688fe669ffd266e7b7960d
alina-amelevich <alinaotto10@gmail.com>
2021-10-17 14:31:28 +0300

    Revert "Привела код в порядок соотв с видео 3.5"
    This reverts commit 3053838d25d3e5cd09ebb69bb961105bd71b04e2.

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 'use strict'
 
-const $arenas = document.querySelector('.arenas');
-
 const player1 = {
-    player: 1,
+    class: 'player1',
     name: 'LIUKANG',
     hp: 80,
     img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
@@ -14,7 +12,7 @@ const player1 = {
 }
 
 const player2 = {
-    player: 2,
+    class: 'player2',
     name: 'KITANA',
     hp: 90,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
@@ -24,37 +22,36 @@ const player2 = {
     }
 }
 
-function createElement(tag, className) {
-    const $tag = document.createElement(tag);
-    if (className) {
-        $tag.classList.add(className);
-    }
-
-    return $tag;
-}
-
 function createPlayer(playerObj) {
-    const $player = createElement('div', 'player' + playerObj.player);
-    const $progressbar = createElement('div', 'progressbar');
-    const $character = createElement('div', 'character');
-    const $life = createElement('div', 'life');
-    const $name = createElement('div', 'name');
-    const $img = createElement('img');
+    const $player = document.createElement('div');
+    $player.classList.add(playerObj.class);
 
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+
+    const $life = document.createElement('div');
+    $life.classList.add('life');
     $life.style.width = playerObj.hp + '%';
-    $name.innerText = playerObj.name;
-    $img.src = playerObj.img;
-    
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
 
-    $character.appendChild($img);
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $name.innerText = playerObj.name;
+
+    const $img = document.createElement('img');
+    $img.src = playerObj.img;
     
     $player.appendChild($progressbar);
     $player.appendChild($character);
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+    $character.appendChild($img);
 
     return $player;
 }
 
+const $arenas = document.querySelector('.arenas');
     $arenas.appendChild(createPlayer(player1));
     $arenas.appendChild(createPlayer(player2));
