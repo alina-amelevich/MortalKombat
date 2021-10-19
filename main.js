d637ef8989ef861d593a6bbc58deb35028582236
alina-amelevich <alinaotto10@gmail.com>
2021-10-19 21:28:20 +0300

    Правки кода в соотв с видео 04.04

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const $randomButton = document.querySelector('.button');
 
 const player1 = {
     player: 1,
-    name: 'LIUKANG',
+    name: 'LIU KANG',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
     weapon: ['machete'],
     attack: function() {
-        console.log(this.name + ' ' + 'Fight...')
+        console.log(`${this.name} fight...`);
     }
 };
 
@@ -21,7 +21,7 @@ const player2 = {
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['blade', 'gun'],
     attack: function() {
-        console.log(this.name + ' ' + 'Fight...')
+        console.log(`${this.name} fight...`);
     }
 };
 
@@ -35,14 +35,14 @@ function createElement(tag, className) {
 }
 
 function createPlayer(playerObj) {
-    const $player = createElement('div', 'player' + playerObj.player);
+    const $player = createElement('div', `player${playerObj.player}`);
     const $progressbar = createElement('div', 'progressbar');
     const $character = createElement('div', 'character');
     const $life = createElement('div', 'life');
     const $name = createElement('div', 'name');
     const $img = createElement('img');
 
-    $life.style.width = playerObj.hp + '%';
+    $life.style.width = `${playerObj.hp}%`;
     $name.innerText = playerObj.name;
     $img.src = playerObj.img;
     
@@ -58,53 +58,52 @@ function createPlayer(playerObj) {
 }
 
 function changeHp(playerObj) {
-    const $playerLife = document.querySelector('.player' + playerObj.player + ' .life');
+    const $playerLife = document.querySelector(`.player${playerObj.player} .life`);
 
     playerObj.hp -= getRandomeDamage(1, 20);
-    console.log('У ' + playerObj.name + ' осталось ' + playerObj.hp + '');
+    
+    // console.log(`У ${playerObj.name} осталось ${playerObj.hp} hp`);
 
     if (playerObj.hp <= 0) {
-        $playerLife.style.width = 0 + '%';
+        playerObj.hp = 0;
+    }
+
+    $playerLife.style.width = `${playerObj.hp}%`;
+
+}
+
+function playerWins(name) {
+    const $winTitle = createElement('div', 'winTitle');
+
+    if (name) {
+        $winTitle.innerText = `${name} wins!`;
     } else {
-        $playerLife.style.width = playerObj.hp + '%';
+        $winTitle.innerText = 'Dead heat';
     }
+
+    return $winTitle;
 }
-
-function whoWon() {
-    function playerWin(winMessage) {
-        const $winTitle = createElement('div', 'winTitle');
-        $winTitle.innerText = winMessage;
-        
-        return $winTitle;
-    }
-
-    //объекты player1/2 из глобальной области видимости
-    if (player1.hp <=0 && player2.hp <=0) { 
-        $arenas.appendChild(playerWin('Dead heat'));
-    } else if (player1.hp <=0) { 
-        $arenas.appendChild(playerWin(player2.name + ' won!'));
-    } else if (player2.hp <=0) {
-        $arenas.appendChild(playerWin(player1.name + ' won!'));
-    }
-}
-
 
 function getRandomeDamage(min, max) {
     const randomeDamage = Math.floor(Math.random() * (max - min + 1) + min);
-    console.log('randomeDamage: ' + randomeDamage);
 
     return randomeDamage;
 }
 
 $randomButton.addEventListener('click', function() {
-    console.log('Click random button');
-
     changeHp(player1);
     changeHp(player2);
 
-    if (player1.hp <= 0 || player2.hp <= 0 ) {
+    if (player1.hp === 0 || player2.hp === 0 ) {
         $randomButton.disabled = true;
-        whoWon();
+    }
+
+    if (player1.hp === 0 && player1.hp < player2.hp) {
+        $arenas.appendChild(playerWins(player2.name));
+    } else if (player2.hp === 0 && player2.hp < player1.hp) {
+        $arenas.appendChild(playerWins(player1.name));
+    } else if (player1.hp === 0 && player2.hp === 0) {
+        $arenas.appendChild(playerWins());
     }
 });
 
