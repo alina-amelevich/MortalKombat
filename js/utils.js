09953f66e21c0819a86f1c1198d445aeb99d2476
alina-amelevich <alinaotto10@gmail.com>
2022-02-16 20:37:47 +0300

    Created JSDoc descriptions for methods

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,12 @@
 
 export class Utils {
 
+  /**
+   * Метод создает HTML-элементы и присваиивает css-класс
+   * @param {string} tag - название тега, который будет создан
+   * @param {string} className - название css-класса, который будет присвоен тэгу
+   * @returns {HTMLElement} HTML-элемент с привоенным css-классом
+   */
   static createElement(tag, className) {
     const $tag = document.createElement(tag);
 
@@ -12,6 +18,9 @@ export class Utils {
     return $tag;
   }
 
+  /**
+   * @returns {string} Текущее время в формате чч:мм
+   */
   static getTime() {
     const date = new Date();
     const formalizeTime = (number) => `${(number >= 10) ? '' : '0'}${number}`;
@@ -20,6 +29,11 @@ export class Utils {
     return time;
   }
 
+  /**
+   * Метод возвращает случайное целое число в промежутке от 1 до переданного значениия параметра max
+   * @param {number} max - верхняя граниица промежутка, в котором необходимо получить рандомное число
+   * @returns {number} - случайное целое число
+   */
   static getRandome(max) {
     return Math.floor(Math.random() * (max - 1 + 1) + 1);
   }
