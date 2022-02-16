7d5517a6a5c50252d69c76d0f95660c3e2eeedf6
alina-amelevich <alinaotto10@gmail.com>
2022-02-16 20:57:03 +0300

    created JSDoc descriptions for methods

--- js/create.js
+++ js/create.js
@@ -4,6 +4,12 @@ import { Utils } from './utils.js';
 
 export class Creator {
 
+  /**
+   * Метод создает и возвращает HTML-элемент игрока, содержащиий HTML-элементы,
+   * необходимые для отображения изображения игрока, его имени и шкалы жизни
+   * @param {object} playerObj - объект игрока
+   * @returns  {HTMLElement} - HTML-элемент игрока
+   */
   static createPlayer(playerObj) {
     const { player, name, hp, img } = playerObj;
 
@@ -27,6 +33,12 @@ export class Creator {
     return $player;
   }
 
+  /**
+   * Метод создает и возращает HTML-элемент, в который записывается соответствующий текст
+   * в зависимости от итогов игры, которые определяются наличием параметра name
+   * @param {string} name - имя игрока-победителя
+   * @returns {HTMLElement} - HTML-элемент, содержащиий сообщение об итогах игры
+   */
   static createWinTitle(name) {
     const $winTitle = Utils.createElement('div', 'winTitle');
 
@@ -39,6 +51,9 @@ export class Creator {
     return $winTitle;
   }
 
+  /**
+   * @returns {HTMLElement} - HTML-элемент содежащий кнопку Restart
+   */
   static createReloadButton() {
     const $reloadWrap = Utils.createElement('div', 'reloadWrap');
     const $reloadButton = Utils.createElement('button', 'button');
